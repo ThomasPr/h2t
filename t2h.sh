565aea0a4275e4d3b4fbd0c8e7342051a70082af
Thomas Preißler <thomas@preissler.me>
2014-12-25 17:33:50 +0100

    added t2h script

---
+++ t2h.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+self="xxx.dip0.t-ipconnect.de"   # eigenen Hostname eintragen, sollte identisch zum h2t-script sein
+cd /root/h2t/                    # das Verzeichnis, in dem das Repo gecloned wurde
+
+host="h2t.preissler.me"
+
+git pull --quiet
+
+for ip_version in 4 6   # ändern falls nur IPv4 oder IPv6 zur Verfügung steht
+do
+  mtr_dir="${self}_${host}_v$ip_version"
+  [ -d "$mtr_dir" ] || mkdir "$mtr_dir"
+  mtr --report --report-wide --report-cycles 1000 --psize 1024 -$ip_version "$host" > "$mtr_dir"/"$(date +%F_%H-%M-%S)" &
+done
+
+wait
+
+git add --all
+git commit --message "added mtrs from $self" --quiet
+git pull --quiet
+git push --quiet
