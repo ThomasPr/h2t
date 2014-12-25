5af386fbea3fa632119b69eadb98e945c03a5f62
Thomas Preißler <thomas@preissler.me>
2014-12-25 17:18:14 +0100

    support for ipv4 and ipv6

--- h2t.sh
+++ h2t.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-hosts="217.86.191.73 h2t.daniel-weber.eu"
+hosts_v4="217.86.191.73 h2t.daniel-weber.eu"
+hosts_v6="h2t.daniel-weber.eu"
+
 self="h2t.preissler.me"
 cd /root/h2t/
 
 git pull --quiet
 
-for host in $hosts
+for ip_version in 4 6
 do
-  mtr_dir="${self}_${host}"
-  [ -d "$mtr_dir" ] || mkdir "$mtr_dir"
-  mtr --report --report-wide --report-cycles 1000 --psize 1024 "$host" > "$mtr_dir"/"$(date +%F_%H-%M-%S)" &
+  hosts=hosts_v$ip_version
+  for host in ${!hosts}
+  do
+    mtr_dir="${self}_${host}_v$ip_version"
+    [ -d "$mtr_dir" ] || mkdir "$mtr_dir"
+    mtr --report --report-wide --report-cycles 1000 --psize 1024 -$ip_version "$host" > "$mtr_dir"/"$(date +%F_%H-%M-%S)" &
+  done
 done
 
 wait
