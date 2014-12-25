7438cad596fb4cbae17f07ddb5a3909c6a7d1209
Thomas Preißler <thomas@preissler.me>
2014-12-25 20:20:03 +0100

    added ipv4 and ipv6 host

--- h2t.sh
+++ h2t.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hosts_v4="217.86.191.73 h2t.daniel-weber.eu 80.147.118.52 80.152.244.206 80.147.49.10 80.147.42.45 87.139.29.180 87.139.62.124 80.147.118.55"
-hosts_v6="h2t.daniel-weber.eu"
+hosts_v4="217.86.191.73 h2t.daniel-weber.eu 80.147.118.52 80.152.244.206 80.147.49.10 80.147.42.45 87.139.29.180 87.139.62.124 80.147.118.55 089-asb52-vdsl50.spdns.de"
+hosts_v6="h2t.daniel-weber.eu 089-asb52-vdsl50.spdns.de"
 
 self="h2t.preissler.me"
 cd /root/h2t/
