586b010bc158beb3e52213901783c0a8af0c6246
Daniel Weber <dw4817@web.de>
2015-01-27 22:16:12 +0100

    removed h2t.daniel-weber.eu
    due to ICMP rate limits on local router with impact on my smokeping
    graphics

--- h2t.sh
+++ h2t.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hosts_v4="freyr.kunesch.net h2t.daniel-weber.eu 80.147.118.52 80.152.244.206 80.147.49.10 80.147.42.45 87.139.29.180 87.139.62.124 80.147.118.55 089-asb52-vdsl50.spdns.de v4cohsrt.mine.nu kunde-01.die-bauer.de kunde-02.die-bauer.de kunde-03.die-bauer.de kunde-04.die-bauer.de firewall.remko.de mpf.ff-leopoldshoehe.de fms.it-kessler.de"
-hosts_v6="h2t.daniel-weber.eu 089-asb52-vdsl50.spdns.de v6cohsrt.mine.nu"
+hosts_v4="freyr.kunesch.net 80.147.118.52 80.152.244.206 80.147.49.10 80.147.42.45 87.139.29.180 87.139.62.124 80.147.118.55 089-asb52-vdsl50.spdns.de v4cohsrt.mine.nu kunde-01.die-bauer.de kunde-02.die-bauer.de kunde-03.die-bauer.de kunde-04.die-bauer.de firewall.remko.de mpf.ff-leopoldshoehe.de fms.it-kessler.de"
+hosts_v6="089-asb52-vdsl50.spdns.de v6cohsrt.mine.nu"
 
 self="h2t.preissler.me"
 cd /root/h2t/
